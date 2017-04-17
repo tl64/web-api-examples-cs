e179d98a026659d2885ec8c3aeb78dafb7f3a9a2
aramzham <aramzham@hotmail.com>
2017-04-17 15:02:09 +0400

    httpresponsemessage

--- WebApiExamples/PluralExample1/Controllers/ValuesController.cs
+++ WebApiExamples/PluralExample1/Controllers/ValuesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 
@@ -8,20 +10,24 @@ namespace PluralExample1.Controllers
 {
     public class ValuesController : ApiController
     {
-        private static List<string> data = new List<string>() {"value1", "value2"}; 
+        private static List<string> data = new List<string>() { "value1", "value2" };
         public IEnumerable<string> Get()
         {
             return data;
         }
 
-        public string Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            return data[id];
+            if (id > data.Count - 1) return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Item not found");
+            else return Request.CreateResponse(HttpStatusCode.OK, data[id]);
         }
 
-        public void Post([FromBody] string value)
+        public HttpResponseMessage Post([FromBody] string value)
         {
             data.Add(value);
+            var message = Request.CreateResponse(HttpStatusCode.Created);
+            message.Headers.Location = new Uri($"{Request.RequestUri}/{data.Count - 1}");
+            return message;
         }
 
         public void Put(int id, [FromBody] string value)
