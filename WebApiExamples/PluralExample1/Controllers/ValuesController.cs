d314559b8fe39ee79317ba927010c13ed9e69c5b
aramzham <aramzham@hotmail.com>
2017-04-17 13:59:11 +0400

    example from pluralsight 1

---
+++ WebApiExamples/PluralExample1/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace PluralExample1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private static List<string> data = new List<string>() {"value1", "value2"}; 
+        public IEnumerable<string> Get()
+        {
+            return data;
+        }
+
+        public string Get(int id)
+        {
+            return data[id];
+        }
+
+        public void Post([FromBody] string value)
+        {
+            data.Add(value);
+        }
+
+        public void Put(int id, [FromBody] string value)
+        {
+            data[id] = value;
+        }
+
+        public void Delete(int id)
+        {
+            data.RemoveAt(id);
+        }
+    }
+}
