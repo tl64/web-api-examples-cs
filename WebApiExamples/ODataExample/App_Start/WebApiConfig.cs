5ce544c0d781d709a25ed155d0521d9f22cb5515
aramzham <aramzham@hotmail.com>
2017-04-23 22:12:21 +0400

    odata queries

--- WebApiExamples/ODataExample/App_Start/WebApiConfig.cs
+++ WebApiExamples/ODataExample/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using ODataExample.Models;
 
-namespace AuthenticationInWebAPI
+namespace ODataExample
 {
     public static class WebApiConfig
     {
@@ -16,6 +18,14 @@ namespace AuthenticationInWebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+
+            //new route won’t use the /api route, but instead will start with odata
+            ODataModelBuilder modelBuilder = new ODataConventionModelBuilder();
+            modelBuilder.EntitySet<Post>("Posts");
+            Microsoft.Data.Edm.IEdmModel model = modelBuilder.GetEdmModel();
+            config.Routes.MapODataRoute("ODataRoute", "odata", model);
+            config.EnableQuerySupport();
         }
     }
 }
